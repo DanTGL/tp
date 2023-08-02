8a6b60a36e9109adff69900a2b766eee4d8dc0f1
TakaRikka <TakaRikka@users.noreply.github.com>
2023-08-02 08:48:19 +0000

    deploy: 24161e34d27f70ab54d584769b1d9f71262d02e9

--- html/search/enums_e.js
+++ html/search/enums_e.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['resarcmessage_0',['ResArcMessage',['../JASResArcLoader_8h.html#a1377a217d616a86a8fdd14f0fccb32fe',1,'JASResArcLoader.h']]]
+  ['regid_0',['RegID',['../structJASRegisterParam.html#aa1f04d37f2825f6dd459e365e257bc09',1,'JASRegisterParam']]],
+  ['resarcmessage_1',['ResArcMessage',['../JASResArcLoader_8h.html#a1377a217d616a86a8fdd14f0fccb32fe',1,'JASResArcLoader.h']]]
 ];
